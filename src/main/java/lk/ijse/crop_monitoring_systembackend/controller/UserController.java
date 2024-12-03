54e3d51704a2bdbf42827b13cef4ee0381db077f
nadeesamaraweera <nadeesamaraweera2000@gmail.com>
2024-12-03 12:45:27 +0530

    create the UserController

---
+++ src/main/java/lk/ijse/crop_monitoring_systembackend/controller/UserController.java
@@ -0,0 +1,71 @@
+package lk.ijse.crop_monitoring_systembackend.controller;
+
+import lk.ijse.crop_monitoring_systembackend.dto.UserDTO;
+import lk.ijse.crop_monitoring_systembackend.exception.DataPersistFailedException;
+import lk.ijse.crop_monitoring_systembackend.exception.NotFoundException;
+import lk.ijse.crop_monitoring_systembackend.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class UserController {
+    @Autowired
+    private UserService userService;
+//    private final PasswordEncoder passwordEncoder;
+
+    private static final Logger logger = Logger.getLogger(UserController.class.getName());
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping(value = "/{email}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> updateUser(@PathVariable("email") String email, @RequestBody UserDTO user) {
+        if (email != null && user != null) {
+            try {
+               // user.setPassword(passwordEncoder.encode(user.getPassword()));
+                userService.updateUser(email, user);
+                logger.info("User password updated successfully: " + user);
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            } catch (NotFoundException e) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            } catch (DataPersistFailedException e) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            } catch (Exception e) {
+                logger.severe("Failed to update user: " + user);
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping(value = "/{email}")
+    public ResponseEntity<Void> deleteUser(@PathVariable("email") String email) {
+        if (email != null) {
+            try {
+                boolean deleted = userService.deleteUser(email);
+                if (deleted) {
+                    logger.info("User deleted successfully with email: " + email);
+                    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+                } else {
+                    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+                }
+            } catch (NotFoundException e) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            } catch (Exception e) {
+                logger.severe("Failed to delete user with email: " + email);
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+}
