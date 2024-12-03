14302166b053063adf3aaa18d11068bddf48ecfa
nadeesamaraweera <nadeesamaraweera2000@gmail.com>
2024-12-03 11:07:54 +0530

    add the logstaff functions and generate the logstaff id

--- src/main/java/lk/ijse/crop_monitoring_systembackend/service/impl/LogServiceIMPL.java
+++ src/main/java/lk/ijse/crop_monitoring_systembackend/service/impl/LogServiceIMPL.java
@@ -1,14 +1,10 @@
 package lk.ijse.crop_monitoring_systembackend.service.impl;
 
-import lk.ijse.crop_monitoring_systembackend.dao.FieldDAO;
-import lk.ijse.crop_monitoring_systembackend.dao.FieldStaffDAO;
-import lk.ijse.crop_monitoring_systembackend.dao.LogDAO;
-import lk.ijse.crop_monitoring_systembackend.dao.StaffDAO;
+import lk.ijse.crop_monitoring_systembackend.dao.*;
 import lk.ijse.crop_monitoring_systembackend.dto.FieldDTO;
 import lk.ijse.crop_monitoring_systembackend.dto.LogDTO;
-import lk.ijse.crop_monitoring_systembackend.entity.FieldStaffEntity;
-import lk.ijse.crop_monitoring_systembackend.entity.LogEntity;
-import lk.ijse.crop_monitoring_systembackend.entity.StaffEntity;
+import lk.ijse.crop_monitoring_systembackend.dto.StaffLogDTO;
+import lk.ijse.crop_monitoring_systembackend.entity.*;
 import lk.ijse.crop_monitoring_systembackend.exception.NotFoundException;
 import lk.ijse.crop_monitoring_systembackend.service.LogService;
 import lk.ijse.crop_monitoring_systembackend.util.MappingUtil;
@@ -35,6 +31,8 @@ public class LogServiceIMPL implements LogService {
     @Autowired
     private FieldStaffDAO fieldStaffDAO;
 
+    @Autowired
+    private StaffLogDAO staffLogDAO;
 
     @Autowired
     private StaffDAO staffDAO;
@@ -57,11 +55,36 @@ public class LogServiceIMPL implements LogService {
                 .map(StaffEntity::getStaffId)
                 .collect(Collectors.toList()));
 
+        for (String staffId : fieldDTO.getStaffs()) {
+            StaffLogDTO staffLogDTO = new StaffLogDTO();
+            staffLogDTO.setStaffLogId(generateStaffLogID());
+            staffLogDTO.setLogId(logId);
+            staffLogDTO.setStaffId(staffId);
+            StaffLogEntity staffLogEntity = staffLogConvertToEntity(staffLogDTO);
+            staffLogDAO.save(staffLogEntity);
+
+            System.out.println("Staff Log saved for staff: " + staffId);
+        }
 
         System.out.println("Log saved successfully: " + log);
     }
 
+    public StaffLogEntity staffLogConvertToEntity(StaffLogDTO staffLogDTO) {
+        StaffLogEntity staffLogEntity = new StaffLogEntity();
+        staffLogEntity.setStaffLogId(staffLogDTO.getStaffLogId());
 
+        if (staffLogDTO.getStaffId() != null) {
+            StaffEntity staffEntity = staffDAO.getReferenceById(staffLogDTO.getStaffId());
+            staffLogEntity.setStaffEntity(staffEntity);
+        }
+
+        if (staffLogDTO.getLogId() != null) {
+            LogEntity logEntity = logDAO.getReferenceById(staffLogDTO.getLogId());
+            staffLogEntity.setLogEntity(logEntity);
+        }
+
+        return staffLogEntity;
+    }
 
     @Override
     public void updateLog(String id, LogDTO log) {
@@ -76,25 +99,39 @@ public class LogServiceIMPL implements LogService {
 
             logDAO.save(tmpLogEntity.get()); // Save the updated log details
 
+            staffLogDAO.deleteByLogEntity_LogId(id); // Delete old staff logs for this log
 
             List<FieldStaffEntity> fieldStaffEntities = fieldStaffDAO.findByField_FieldId(log.getFieldId());
             List<String> staffIds = fieldStaffEntities.stream()
                     .map(FieldStaffEntity::getStaff)
                     .map(StaffEntity::getStaffId)
                     .collect(Collectors.toList());
+            for (String staffId : staffIds) {
+                StaffLogDTO staffLogDTO = new StaffLogDTO();
+                staffLogDTO.setLogId(id);
+                staffLogDTO.setStaffId(staffId);
 
-        System.out.println("Log updated successfully: " + log);
-    } else {
-        System.out.println("Log not found with id: " + id);
-        throw new NotFoundException("Log not found with id: " + id);
+                // Manually assign an ID if necessary
+                StaffLogEntity staffLogEntity = staffLogConvertToEntity(staffLogDTO);
+                staffLogEntity.setStaffLogId(generateStaffLogID());
+
+                staffLogDAO.save(staffLogEntity);
+            }
+
+            System.out.println("Log updated successfully: " + log);
+        } else {
+            System.out.println("Log not found with id: " + id);
+            throw new NotFoundException("Log not found with id: " + id);
+        }
     }
-}
 
 
     @Override
     public LogDTO searchLog(String id) {
         if (logDAO.existsById(id)) {
+            List<StaffLogEntity> byLogEntityLogId = staffLogDAO.findByLogEntity_LogId(id);
             LogDTO logDTO = mappingUtil.logConvertToDTO(logDAO.getReferenceById(id));
+            logDTO.setStaff(byLogEntityLogId.stream().map(StaffLogEntity::getStaffEntity).map(StaffEntity::getStaffId).collect(Collectors.toList()));
             return logDTO;
         } else {
             System.out.println("Log not found with id: " + id);
@@ -106,6 +143,7 @@ public class LogServiceIMPL implements LogService {
     public boolean deleteLog(String id) {
         if (logDAO.existsById(id)) {
             logDAO.deleteById(id);
+            staffLogDAO.deleteByLogEntity_LogId(id);
             System.out.println("Log deleted successfully with id: " + id);
             return true;
         } else {
@@ -117,7 +155,9 @@ public class LogServiceIMPL implements LogService {
     @Override
     public List<LogDTO> getAllLogs() {
         return logDAO.findAll().stream().map(logEntity -> {
+            List<StaffLogEntity> byLogEntityLogId = staffLogDAO.findByLogEntity_LogId(logEntity.getLogId());
             LogDTO logDTO = mappingUtil.logConvertToDTO(logEntity);
+            logDTO.setStaff(byLogEntityLogId.stream().map(StaffLogEntity::getStaffEntity).map(StaffEntity::getStaffId).collect(Collectors.toList()));
             return logDTO;
         }).collect(Collectors.toList());
     }
@@ -138,5 +178,19 @@ public class LogServiceIMPL implements LogService {
         }
     }
 
-
+    private String generateStaffLogID() {
+        if (staffLogDAO.count() == 0) {
+            return "SL001";
+        } else {
+            String lastId = staffLogDAO.findAll().get(staffLogDAO.findAll().size() - 1).getStaffLogId();
+            int newId = Integer.parseInt(lastId.substring(2)) + 1;
+            if (newId < 10) {
+                return "SL00" + newId;
+            } else if (newId < 100) {
+                return "SL0" + newId;
+            } else {
+                return "SL" + newId;
+            }
+        }
+    }
 }
