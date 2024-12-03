b14dded8800009e4effed552bb2f69ed59d84ac4
nadeesamaraweera <nadeesamaraweera2000@gmail.com>
2024-12-03 09:43:29 +0530

    create the HealthController for run the app

---
+++ src/main/java/lk/ijse/crop_monitoring_systembackend/controller/HealthController.java
@@ -0,0 +1,19 @@
+package lk.ijse.crop_monitoring_systembackend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping("/api/v1/health")
+public class HealthController {
+    private static final Logger logger = Logger.getLogger(HealthController.class.getName());
+
+    @GetMapping
+    public String checkHealth(){
+        logger.info("Crop Monitoring System API is Running");
+        return "Crop Monitoring System API is Running";
+    }
+}
